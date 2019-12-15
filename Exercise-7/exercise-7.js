26b8baaa50883ad1bdcf848a556b5acebf25ef95
akbarakma <akbarajo1234@gmail.com>
2019-12-15 15:08:59 +0700

    pada blm selesai

---
+++ Exercise-7/exercise-7.js
@@ -0,0 +1,14 @@
+/*ROW 1 */
+var row1 = 5;
+for(var i = 1; i<=row1; i++){
+    console.log('*');
+}
+
+/*ROW 2 */
+var row2 = 5;
+for(var i = 1; i<=row2; i++){
+    for(var j = 1; j<=row2; j++){
+        console.log('*');
+    }
+    console.log('/')
+}
