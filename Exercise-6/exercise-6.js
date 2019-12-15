c688909a134e26f52abd04ab29bc995df20d5e69
akbarakma <akbarajo1234@gmail.com>
2019-12-15 19:38:13 +0700

    exercise 6 selesai

--- Exercise-6/exercise-6.js
+++ Exercise-6/exercise-6.js
@@ -26,3 +26,33 @@ console.log('Looping kedua:')
 for(var i = 20; i>=1; i--){
     console.log(i + '- I will become fullstack developer!')
 }
+
+/*SOAL 3 */
+console.log('Tugas 3')
+console.log('Ganjil Genap')
+for(var i = 1 ; i <= 100 ; i++){
+    if(i % 2 === 0){
+        console.log('Genap');
+    }
+    else{
+        console.log('Ganjil');
+    }
+}
+console.log('Pertambahan counter 2')
+for(var i = 1 ; i <= 100 ; i+=2){
+    if(i % 3 === 0){
+        console.log(i + ' kelipatan 3')
+    }
+}
+console.log('Pertambahan counter 5')
+for(var i = 1 ; i <= 100 ; i+=5){
+    if(i % 6 === 0){
+        console.log(i + ' kelipatan 6')
+    }
+}
+console.log('Pertambahan counter 9')
+for(var i = 1 ; i <= 100 ; i+=9){
+    if(i % 10 === 0){
+        console.log(i + ' kelipatan 10')
+    }
+}
