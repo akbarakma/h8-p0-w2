76ab96318f6f1fe05b04122e6b8c51b302fd86f6
akbarakma <akbarajo1234@gmail.com>
2019-12-15 14:49:17 +0700

    exercise 6 blm kelar

---
+++ Exercise-6/exercise-6.js
@@ -0,0 +1,27 @@
+/* SOAL 1 */
+var loop1 = 2;
+var loop2 = 20;
+
+console.log('Looping pertama: ')
+while(loop1 <= 20){
+    console.log(loop1 + '- I love coding!');
+    loop1+=2;
+}
+
+console.log('Looping kedua: ')
+while(loop2 >= 2){
+    console.log(loop2 + '- I will become fullstack developer!');
+    loop2-=2;
+}
+
+/*SOAL 2 */
+
+console.log('Looping pertama:');
+for(var i = 1; i<=20; i++){
+    console.log(i + '- I love coding!')
+}
+
+console.log('Looping kedua:')
+for(var i = 20; i>=1; i--){
+    console.log(i + '- I will become fullstack developer!')
+}
