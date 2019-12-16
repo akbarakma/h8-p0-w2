dfe18773793d67848432018277331ccb594963c2
akbarakma <akbarajo1234@gmail.com>
2019-12-16 10:00:29 +0700

    selesai yeay

--- Exercise-7/exercise-7.js
+++ Exercise-7/exercise-7.js
@@ -1,20 +1,17 @@
 /*ROW 1 */
 console.log('Tugas 1')
 var row1 = 5;
-for(var i = 1; i<=row1; i++){
+for(var i = 0 ; i <= row1 ; i++){
     console.log('*');
 }
 
-/*ROW 2 */
-console.log('Tugas 2')
+//ROW 2
 var row2 = 5;
-var bintang = '*';
-for (var i = 1 ; i <= row2 ; i++){
-    console.log(bintang)
-    bintang = bintang + '*'  
-    for(var j = 1 ; j <= row2 ; j++){
-          
-        console.log(bintang)
+var bintang = '';
+for(var i = 0 ; i <= row2 ; i++){
+    for(var j = 0 ; j <= row2 ; j++){
+        bintang += '*';
     }
-    
+    console.log(bintang);
+    bintang = '\n';
 }
