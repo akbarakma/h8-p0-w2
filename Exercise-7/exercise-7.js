1afd23cc50e0e4f92331490ec2fd6bff9e19372f
akbarakma <akbarajo1234@gmail.com>
2019-12-17 15:16:33 +0700

    update

--- Exercise-7/exercise-7.js
+++ Exercise-7/exercise-7.js
@@ -1,7 +1,7 @@
 /*ROW 1 */
 console.log('Tugas 1')
 var row1 = 5;
-for(var i = 0 ; i <= row1 ; i++){
+for(var i = 1 ; i <= row1 ; i++){
     console.log('*');
 }
 
@@ -9,7 +9,7 @@ for(var i = 0 ; i <= row1 ; i++){
 console.log('Tugas 2')
 var row2 = 5;
 var bintang = '';
-for(var i = 0 ; i <= row2 ; i++){
+for(var i = 1 ; i <= row2 ; i++){
     for(var j = 0 ; j <= row2 ; j++){
         bintang += '*';
     }
