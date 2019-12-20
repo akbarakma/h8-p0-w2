285385c6b63e2aae06db1571e4fc3255b98b497f
akbarakma <akbarajo1234@gmail.com>
2019-12-20 18:20:48 +0700

    blm selesai

--- soal liburan native/soal liburan part 1/2.js
+++ soal liburan native/soal liburan part 1/2.js
@@ -16,7 +16,7 @@
   cars ke - 0 = butuh waktu 2,5 jam
   cars ke - 1 = butuh waktu 2,25 jam
   cars ke - 2 = butuh waktu 2.35 jam
-  cars ke - 3 = butuh waktu 6.3 jam
+  cars ke - 3 = butuh waktu 0.3 jam
 
   [OUTPUT]
   [ 'B 1174 BA', 'B 1172 BA', 'B 1173 BA', 'B 1171 BA' ]
@@ -28,7 +28,24 @@
 */
 
 function winner (cars, totalKM) {
-  //code below here
+  hasil = [];
+  waktu = [];
+  for(var i = 0 ; i < cars.length ; i++){
+    var jarak = totalKM - cars[i][2];
+    //waktu[waktu.length] = jarak / cars[i][1];
+    waktu[i] = jarak / cars[i][1];
+    for(var j = 0 ; j < cars.length ; j++){
+      
+      if(waktu[i] <= waktu[j]){
+        waktu[i] = waktu[j];
+      }
+    }
+    hasil[hasil.length] = waktu[i];
+  }
+  //waktu.sort().reverse(); //CARI LAGI BUBBLE SORT
+  return hasil;
+
+
 };
 
 console.log(winner([['B 1171 BA', 100, 200], ['B 1172 BA', 200, 0], ['B 1173 BA', 98, 220], ['B 1174 BA', 80, 400]], 450));
