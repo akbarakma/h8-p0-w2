8090530c0af87c6acde2aedf4a5771b01998ebaa
akbarakma <akbarajo1234@gmail.com>
2019-12-24 22:53:50 +0700

    blm selesai

--- soal liburan native/soal liburan part 1/3.js
+++ soal liburan native/soal liburan part 1/3.js
@@ -31,7 +31,47 @@
 */
 
 function mostCarsByState (cars) {
-  //code below here
+  var jumlah = cars.length;
+  hasil = []
+  var a = 0;
+  var b = 0;
+  var c = 0;
+  var d = 0;
+  for(var i = 0 ; i < jumlah ; i ++){
+    if(cars[i] === 'A'){
+      a ++;
+    }
+    if(cars[i] === 'B'){
+      b ++;
+    }
+    if(cars[i] === 'C'){
+      c ++;
+    }
+    if(cars[i] === 'D'){
+      d ++;
+    }
+  }
+  a = (a / jumlah)*100;
+  b = (b / jumlah)*100;
+  c = (c / jumlah)*100;
+  d = (d / jumlah)*100;
+  hasil.push(['A'],[a]);
+  hasil.push(['B'],[b]);
+  hasil.push(['C'],[c]);
+  hasil.push(['D'],[d]);
+
+  for(var i = 0 ; i < hasil.length ; i++){
+    for(var j = 0 ; j < hasil.length - i - 1 ; j ++){
+      if(hasil[j][1] < hasil[j+1][1]){
+        var sementara = hasil[j]
+        hasil[j] = hasil[j+1]
+        hasil[j+1] = sementara;
+      }
+    }
+  }
+
+
+  return hasil;
 };
 
 console.log(mostCarsByState(['B', 'D', 'B', 'B', 'A', 'C', 'D']));
